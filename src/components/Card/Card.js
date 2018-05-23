67d9150ff60884ce88d0ab9146a49d00d90505ce
Kashi Samaraweera <kashi@kashis.com.au>
2018-05-23 02:51:24 +0200

    WIP Error boundaries implementation for Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import './../Card/Card.css';
 
+/**
+ * This abstract Card component is used to isolate discrete Cards in an an app
+ * that arranges a series of Cards. This is used to manage basic Card styling
+ * as well as error handling.
+ **/
 class Card extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    console.error(error);
+  }
+
   render() {
+    if (this.state.hasError) return <article role="main"><h1>Error</h1></article>
     return <article { ...this.props } role="main">{this.props.children}</article>
   }
 }
--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,9 +1,94 @@
 import React, { Component } from 'react';
 import Card from './../Card/Card';
 import Player from './../Player/Player';
+import ReactPlaceholder from 'react-placeholder';
+import renderHTML from 'react-render-html';
 import './EventCard.css';
+import "react-placeholder/lib/reactPlaceholder.css";
+
+/** Interval between refreshes (in milliseconds) */
+const REFRESH_CARD_INTERVAL = 10000;
+
+/** Endpoint to hit for JSON data. */
+const EVENT_DATA_URL = '/data/events.json';
+
+/** Endpoint to hit for JSON data. */
+const EVENT_SUBSCRIPTION_URL = '/subscriptions';
+
+/** Stores a "loading" state for re-use whilst retreiving data. */
+const LOADING_STATE = {
+  readyState: 0,
+  publication: {
+    name: '--'
+  },
+  name: <ReactPlaceholder rows={1} ready={false}>...</ReactPlaceholder>,
+  description: <ReactPlaceholder rows={3} ready={!!0}>...</ReactPlaceholder>,
+  image: "",
+  startDate: "",
+  duration: "",
+  performer: ""
+};
 
 class EventCard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = LOADING_STATE;
+  }
+
+  /**
+   * Request data from our JSON endpoint once the component has mounted.
+   */
+  async componentDidMount() {
+    this.updateFromSource();
+  }
+
+  /**
+   * Performs a Fetch for the JSON endpoint to read the event data.
+   */
+  updateFromSource = async () => {
+    this.setState(LOADING_STATE);
+    this.setState({readyState: 2});
+    let publicationJson = await fetch(EVENT_DATA_URL);
+    this.setState({readyState: 3});
+    let publication = await publicationJson.json().catch(function() { throw new Error("Could not parse JSON for event data.")});
+    this.setState({ publication: publication });
+    let events = publication.releasedEvent;
+    let randomEventIndex = Math.floor(Math.random() * events.length);
+    this.displayEvent(events[randomEventIndex], 0);
+  }
+
+  /**
+   * Submits a POST request to initiate a subscription to a specified
+   * Event.
+   * 
+   * @param {string} eventID  The ID of the event to supply to the
+   *                          server.
+   */
+  subscribe = async (eventID) => {
+    try {
+      const subscribeRequest = await fetch(EVENT_SUBSCRIPTION_URL, {
+        body: JSON.stringify({ eventID: eventID }),
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "content-type": "application/json"
+        },
+        method: "POST",
+        mode: "cors"
+      });
+      let success = JSON.parse(subscribeRequest.text());
+      if (success) this.setState({ subscription: true });
+    } catch (e) {
+      console.warn(`The request to ${EVENT_SUBSCRIPTION_URL} could not be made.`);
+    }
+  }
+
+  displayEvent = (event, cardNumber) => {
+    event.description = renderHTML(`<p>${event.description}</p>`);
+    this.setState({readyState: 4, ...event});
+    setTimeout(this.updateFromSource, REFRESH_CARD_INTERVAL);
+  }
 
   /**
    * Takes a ISO-8601 timestamp and (as a string) and returns the number
@@ -42,21 +127,21 @@ class EventCard extends Component {
 
   render() {
     return (
-      <Card className="Card EventCard" aria-labelledby={`${this.props.cardID}__title`}>
-        <Player aria-labelledby={`${this.props.cardID}__header`} name={this.props.name} image={this.props.image} />
+      <Card className="Card EventCard" aria-labelledby={`${this.state.cardID}__title`}>
+        <Player aria-labelledby={`${this.state.cardID}__header`} name={this.state.name} image={this.state.image} />
         <section className="Card__content" tabIndex="0">
-          <h1 id={`${this.props.cardID}__title`}>{ this.props.name }</h1>
-          { this.props.description }
+          <h1 id={`${this.props.cardID}__title`}>{ this.state.name }</h1>
+          { this.state.description }
         </section>
         <section className="Card__actions" aria-label="event actions">
-          <button className="Button Button--large Button--plus icon-add Card__Cta" onClick={() => this.props.subscribe(this.props["@id"])}>Follow this event</button>
+          <button className="Button Button--large Button--plus icon-add Card__Cta" onClick={() => this.state.subscribe(this.state["@id"])}>Follow this event</button>
         </section>
         <footer className="Card__footer" role="contentinfo">
           <section className="Card__publication-info" aria-label="Event details">
             <ul className="FlatList FlatList--slash FlatList--spread">
-              <li>Presented By <address aria-label="Event presenter">{ this.props.performer }</address></li>
-              <li aria-label="Event time">{ this.timeAgo(this.props.startDate) }</li>
-              <li aria-label="Context"><a title="Tag" rel="tag">{ this.props.publication.name }</a></li>
+              <li>Presented By <address aria-label="Event presenter">{ this.state.performer }</address></li>
+              <li aria-label="Event time">{ this.timeAgo(this.state.startDate) }</li>
+              <li aria-label="Context"><a title="Tag" rel="tag">{ this.state.publication.name }</a></li>
             </ul>
           </section>
           <hr />
